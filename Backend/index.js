3e482cdc61bdd24e1e7d6fd92b44fd0bca679503
Isuru <it21041648@my.sliit.lk>
2023-12-29 01:55:04 +0530

    dot env

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { PORT } from './config.js';
 import mongoose from 'mongoose';
 import { MONGO_URL } from './config.js';
-import productRoute from './routes/productRoute.js';
 import path from 'path';
 import cors from 'cors';
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import productRoute from './routes/productRoute.js';
 
-
-const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 
+//Mongoose setup
 
-mongoose.connect(MONGO_URL)
+const PORT = process.env.PORT ||3000;
+mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
